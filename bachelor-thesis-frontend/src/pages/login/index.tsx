84cd1f02d7fb2a04f268e2c0fdb6f0b7124a0444
David Alexandru <fanefaraon696@gmail.com>
2024-01-12 01:10:48 +0200

    added: material ui login page

--- bachelor-thesis-frontend/src/pages/login/index.tsx
+++ bachelor-thesis-frontend/src/pages/login/index.tsx
@@ -1,6 +1,19 @@
 import { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+}));
 
 const LoginPage = () => {
+  const classes = useStyles();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -18,26 +31,31 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
-      <h1>Login Page</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email:
-          <input type="email" value={email} onChange={handleEmailChange} />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </label>
-        <br />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <form
+      className={classes.root}
+      noValidate
+      autoComplete="off"
+      onSubmit={handleSubmit}
+    >
+      <TextField
+        id="email"
+        label="Email"
+        variant="outlined"
+        value={email}
+        onChange={handleEmailChange}
+      />
+      <TextField
+        id="password"
+        label="Password"
+        type="password"
+        variant="outlined"
+        value={password}
+        onChange={handlePasswordChange}
+      />
+      <Button variant="contained" color="primary" type="submit">
+        Login
+      </Button>
+    </form>
   );
 };
 
