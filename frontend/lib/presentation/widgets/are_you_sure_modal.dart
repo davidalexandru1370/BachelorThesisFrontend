223b55fff426d06293c953fc016bb62832d7b67f
David Alexandru <fanefaraon696@gmail.com>
2024-03-07 21:17:31 +0200

    added: loading progress for on yes pressed

--- frontend/lib/presentation/widgets/are_you_sure_modal.dart
+++ frontend/lib/presentation/widgets/are_you_sure_modal.dart
@@ -1,43 +1,77 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/presentation/extensions/exception_extensions.dart';
 import 'package:frontend/presentation/l10n/app_l10n.dart';
+import 'package:frontend/presentation/widgets/notifications/toast_notification.dart';
 
 class AreYouSureModal {
   static _getDialog(BuildContext dialogContext, BuildContext appContext,
       {Function? onYes = null, Function? onCancel = null}) {
     var localization = Localization().getAppLocalizations(appContext);
-    return Dialog(
-      alignment: Alignment.center,
-      child: SizedBox(
-        height: MediaQuery.of(dialogContext).size.height * 0.5,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(localization!.areYouSure),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-                  onPressed: () {
-                    Navigator.of(dialogContext).pop();
-                  },
-                  child: Text(localization.cancel),
-                ),
-                ElevatedButton(
-                  style:
-                      ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                  onPressed: () {
-                    Navigator.of(dialogContext).pop();
-                  },
-                  child: Text(localization.yes),
-                ),
-              ],
-            ),
-          ],
+    bool _isLoading = false;
+    return StatefulBuilder(
+        builder: (BuildContext stateContext, StateSetter setState) {
+      return Dialog(
+        alignment: Alignment.center,
+        child: SizedBox(
+          height: MediaQuery.of(dialogContext).size.height * 0.25,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Text(localization!.areYouSure,
+                    style: const TextStyle(fontSize: 17)),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                        minimumSize: const Size(125, 50)),
+                    onPressed: () {
+                      Navigator.of(dialogContext).pop();
+                    },
+                    child: Text(
+                      localization.cancel,
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        minimumSize: const Size(125, 50)),
+                    onPressed: () async {
+                      if (_isLoading == true) {
+                        return;
+                      }
+                      if (onYes != null) {
+                        setState(() {
+                          _isLoading = true;
+                        });
+                        try {
+                          await onYes();
+                        } on Exception catch (e) {
+                          ToastNotification.showError(appContext, e.getMessage);
+                        } finally {
+                          setState(() {
+                            _isLoading = false;
+                          });
+                        }
+                      }
+                      Navigator.of(dialogContext).pop();
+                    },
+                    child: _isLoading == false
+                        ? Text(localization.yes)
+                        : const CircularProgressIndicator(),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 
   static displayDialog(BuildContext context,
